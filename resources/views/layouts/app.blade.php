69f63cf04da54fb0b29273408ba5598ad34208dd
okadwaja <okadwaja99@gmail.com>
2025-05-22 21:19:16 +0800

    edit favicon and title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Desa Taman</title>
+    <link rel="icon" href="{{ asset('storage/foto_profil/logo-2.png') }}" type="image/png"/>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Desa Taman</title>
+        <link rel="icon" href="{{ asset('storage/foto_profil/logo-2.png') }}" type="image/png"/>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
